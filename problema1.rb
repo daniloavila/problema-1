c57ecdef771c4b21fe041e53954b65fcf1c2b520
Danilo Ávila <daniloavilaf@gmail.com>
2011-05-27 13:07:03 -0300

    problema 1 feito

---
+++ problema1.rb
@@ -0,0 +1,36 @@
+class Problema1
+
+	attr_reader :numero, :itens
+
+	def initialize
+
+		@itens = 0
+		@numero = 0
+
+		(1..999999).each do |n|
+			sequencia_size = Problema1.calculate n
+			if sequencia_size > @itens
+				@itens = sequencia_size
+				@numero = n
+			end
+		end
+
+	end
+
+	def self.calculate numero
+		cont=1
+
+		while numero != 1 do
+
+			if numero.even?
+				numero /= 2
+				cont += 1
+			else
+				numero = (numero*3 +1)/2
+				cont +=2
+			end
+		end
+
+		cont
+	end
+end
